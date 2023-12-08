85bf5e8f863dc74a431e36a1f8c8766097c62640
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2023-12-08 15:05:49 +0600

    Update A1_190204025.m

--- Assignment_1/A1_190204025.m
+++ Assignment_1/A1_190204025.m
@@ -2,25 +2,14 @@ I = imread('img.jpg');
 figure(1);
 imshow(I);
 
-% Converting to Grayscale
-GI = rgb2gray(I);
-figure(2);
-imshow(GI);
-
 % Creating mirror img
-MiI = GI(:, (end:-1:1), :); 
-% figure(3);
-% imshow(MiI);
+MiI = I(:, (end:-1:1), :); 
+figure(2);
+imshow(MiI);
 
 % Merging the imgs
-[rows, cols] = size(GI);
-MrgImg = zeros(rows, 2 * cols, 'uint8');
-MrgImg(:, 1:cols) = GI;
-MrgImg(:, cols+1:end) = MiI;
+MrgImg = cat(2,I,MiI);
 figure(3);
 imshow(MrgImg);
 
 imwrite(MrgImg,'N:\4.2_Matlab\assignment 1\output.jpg');
-
-
-
